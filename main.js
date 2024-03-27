bdbdfb078780902d2d1058f04db35319b01a7ef2
andip <andiprabandaru@gmail.com>
2024-03-28 02:49:23 +0700

    frist commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// for, for of, for in, foreach
+// while dan do while 
+// MAP
+
+const pacarSaya = ['agnes', 'putri', 'marino', 'anggela', 'vexana']
+
+// for (let pacar of pacarSaya) {
+//     console.log(pacar)
+// }
+
+// for in
+// for (let pacar in pacarSaya) {
+//     console.log(pacar)
+// }
+
+// foreach
+// pacarSaya.forEach((pacar, index ) => {
+//     console.log(pacar, index +1)
+// });
+
+// MAP
+pacarSaya.map((pacar, index ) => {
+    console.log(pacar, index +1)
+});
